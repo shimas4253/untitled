07d0bfe5d64d847abaaec5b6148de1268a4f0c37
shimas4253 <muhammedshimas123@gmail.com>
2022-09-19 16:09:13 +0530

    Initial commit

---
+++ bin/untitled.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+import 'package:untitled/untitled.dart' as untitled;
+
+void main() {
+  String name='shimas';
+  int age=20;
+  double mark=8.8;
+  String mail='muhammedshimas123@gmail.com';
+
+  print('my name is $name');
+  print('i am $age years old');
+  stdout.write('my mark is $mark');
+  stdout.write('\n mail is $mail');
+}
