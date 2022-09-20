dd24900bb8ed5c8a15d02a6101f4c3e7fe823e08
shimas4253 <muhammedshimas123@gmail.com>
2022-09-20 15:59:50 +0530

    commit

---
+++ bin/student.dart
@@ -0,0 +1,29 @@
+class student{
+  String? name;
+  int? age;
+  String? place;
+  int? mobile;
+  static String institution="luninar";
+  static String course="flutter";
+}
+void main(){
+  student student1=student();
+  print("name       :${student1.name ="sathar"}");
+  print("age        :${student1.age =24}");
+  print("place      :${student1.place="manjeri"}");
+  print("mobile     :${student1.mobile =8472274471}");
+  print("institution:${student.institution}");
+  print("course     :${student.course}");
+
+  print("____________________________________");
+  student student2=student();
+  print("name       :${student2.name ="shimas"}");
+  print("age        :${student2.age =21}");
+  print("place      :${student2.place="edavanna"}");
+  print("mobile     :${student2.mobile =363269235}");
+  print("institution:${student.institution}");
+  print("course     :${student.course}");
+
+
+
+}
